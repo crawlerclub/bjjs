2f93d622755513181676cced6de8e13787fa30c2
liuzhanliang <zhanliangliu@qq.com>
2018-05-20 09:01:04 +0800

    Update get.sh

--- bin/get.sh
+++ bin/get.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #  @author:       Zhanliang Liu
-#  @description:  get http://www.bjjs.gov.cn/bjjs/fwgl/fdcjy/fwjy/index.shtml
+#  @description:  get www.bjjs.gov.cn daily
 HOME=/home/zliu
-echo "Starting download http://www.bjjs.gov.cn/bjjs/fwgl/fdcjy/fwjy/index.shtml"
+url="http://www.bjjs.gov.cn/bjjs/fwgl/fdcjy/fwjy/index.shtml"
+echo "Starting download ${url}"
 CURR_PATH=`cd $(dirname $0);pwd;`
 cd $CURR_PATH
 day=`date +%Y%m%d`
 echo $day >> git.log
-wget http://www.bjjs.gov.cn/bjjs/fwgl/fdcjy/fwjy/index.shtml  -O ../data/${day}.html
+wget ${url}  -O ../data/${day}.html
 git add ../data/${day}.html
 git commit -m "get data for ${day}" >> git.log
 git push >> git.log
